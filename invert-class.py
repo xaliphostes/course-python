66405c2b8cf2a72be2ea96c2290deafa50f8b6ad
Xaliphostes <fmaerten@gmail.com>
2024-03-04 09:37:57 +0100

    new versions for python

--- invert-class.py
+++ invert-class.py
@@ -10,53 +10,72 @@ import random as rnd
 import functools
 import numpy as np
 import matplotlib.pyplot as plt
-from typing import Callable
+from abc import abstractmethod
 
-Vector = tuple[float, float]
-Stress = tuple[tuple[float, float], tuple[float, float]]
+type Vector = list[float]
+type Stress = list[Vector]
+
 
 def dot(n1: Vector, n2: Vector) -> float:
-    return n1[0]*n2[0] + n1[1]*n2[1]
+    return n1[0] * n2[0] + n1[1] * n2[1]
 
-def lerp(v0: float, v1: float, t: float) -> float :
+
+def lerp(v0: float, v1: float, t: float) -> float:
     return (1 - t) * v0 + t * v1
 
+
+def normalize(n: Vector) -> Vector:
+    l = math.sqrt(n[0] ** 2 + n[1] ** 2)
+    if l != 0:
+        return [n[0] / l, n[1] / l]
+    else:
+        return n
+
+
 # --------------------------------------------------
+
 
 class RemoteStress:
     __S1: Vector
     __S3: Vector
-    def S1(self) -> Vector: return self.__S1
-    def S3(self) -> Vector: return self.__S3
+
+    def S1(self) -> Vector:
+        return self.__S1
+
+    def S3(self) -> Vector:
+        return self.__S3
+
     def set(self, theta: float, k: float) -> None:
         a = math.radians(theta)
         c, s = math.cos(a), math.sin(a)
-        xx, xy, yy = k*s*s, k*c*s, k*c*c
+        xx, xy, yy = k * s * s, k * c * s, k * c * c
         trace = xx + yy
-        discri = math.sqrt(trace*trace - 4*(xx * yy - xy*xy))
+        discri = math.sqrt(trace * trace - 4 * (xx * yy - xy * xy))
         # Decreasing order according to the eigen values
-        self.__S1 = self.__normalize([xy, (trace + discri) / 2 - xx])
-        self.__S3 = self.__normalize([xy, (trace - discri) / 2 - xx])
-    def __normalize(self, n: Vector) -> Vector:
-        l = math.sqrt(n[0] ** 2 + n[1] ** 2)
-        if l != 0:
-            return [n[0] / l, n[1] / l]
-        else:
-            return n
+        self.__S1 = normalize([xy, (trace + discri) / 2 - xx])
+        self.__S3 = normalize([xy, (trace - discri) / 2 - xx])
+
 
 # --------------------------------------------------
 
 # Class de base (abstraite car ne fait rien)
 class Data:
     n: Vector
+
     def __init__(self, n: Vector) -> None: self.n = n
+
+    @abstractmethod
     def cost(self, r: RemoteStress) -> float: pass
+
     def normal(self): return self.n
+    def setNormal(self, n: Vector): self.n = n
+
 
 # Class dérivée de Data
 class Joint(Data):
     def cost(self, r: RemoteStress) -> float:
         return 1.0 - math.fabs(dot(self.n, r.S3()))
+
 
 # Class dérivée de Data
 class Stylolite(Data):
@@ -83,18 +102,19 @@ def monteCarlo(data: list[Data], n: int = 5000):
             cost, theta, k = c, THETA, K
             print(theta, k, c)
 
+
 def plotDomain(data: list[Data], n: int):
-    Z = np.zeros(shape=(n,n))
+    Z = np.zeros(shape=(n, n))
     min = 0.001
     max = 0.99
     remote = RemoteStress()
     for i in range(0, n):
-        k = lerp(min, max, i/(n-1))
+        k = lerp(min, max, i / (n - 1))
         for j in range(0, n):
-            theta = lerp(0, 180, j/(n-1))
+            theta = lerp(0, 180, j / (n - 1))
             remote.set(theta, k)
-            Z[j][i] = functools.reduce(lambda a, b: a+b, [x.cost(remote) for x in data]) / len(data)
-            
+            Z[j][i] = functools.reduce(lambda a, b: a + b, [x.cost(remote) for x in data]) / len(data)
+
     X, Y = np.meshgrid(np.linspace(min, max, n), np.linspace(0, 180, n))
     levels = np.linspace(Z.min(), Z.max(), 50)
     cmap = 'jet'
@@ -107,18 +127,19 @@ def plotDomain(data: list[Data], n: int):
     fig.colorbar(plt.cm.ScalarMappable(cmap=cmap), ax=ax, orientation='vertical', label='Cost')
     plt.show()
 
+
 def plotRotateS3():
     x = []
-    
+
     yj = []
     ys = []
-    
+
     font = {'family': 'serif',
-        'color':  'darkred',
-        'weight': 'normal',
-        'size': 16,
-        }
-    
+            'color': 'darkred',
+            'weight': 'normal',
+            'size': 16,
+            }
+
     remote = RemoteStress()
     joint = Joint([0, 1])
     stylo = Stylolite([0, 1])
@@ -126,14 +147,16 @@ def plotRotateS3():
     for angle in range(0, 91, 1):
         x.append(angle)
         remote.set(angle, 1)
-        yj.append( joint.cost(remote) )
-        ys.append( stylo.cost(remote) )
+        yj.append(joint.cost(remote))
+        ys.append(stylo.cost(remote))
 
     fig, ax = plt.subplots()
     ax.plot(x, yj, label='Joint')
     ax.plot(x, ys, label='Stylolite')
     ax.legend()
-    ax.axvline(x=45, ymax=0.3, linewidth=1, color='black', linestyle=(0, (5, 5)))
+    px = 1 - math.sqrt(2)/2
+    ax.axvline(x=45, linewidth=1, color='black', linestyle=(0, (5, 5)))
+    ax.axhline(xmax=45, y=px, linewidth=1, color='black', linestyle=(0, (5, 5)))
     ax.set_xlim(0, 90)
     ax.set_ylim(0, 1)
     # ---
@@ -143,19 +166,21 @@ def plotRotateS3():
     # ---
     plt.show()
 
+
 def addData(file: str, dataType: str, data: list[Data]):
-        f = open(file, "r")
-        for line in f: # for each line
-            tokens = line.removesuffix('\n').split(' ')
-            nx = float(tokens[0])
-            ny = float(tokens[1])
-            if dataType == 'joint':
-                data.append( Joint([nx, ny]) )
-            elif dataType == 'stylolite':
-                data.append( Stylolite([nx, ny]) )
-            else:
-                raise Exception('data type {dataType} is unknown!')
-            
+    f = open(file, "r")
+    for line in f:  # for each line
+        tokens = line.removesuffix('\n').split(' ')
+        nx = float(tokens[0])
+        ny = float(tokens[1])
+        if dataType == 'joint':
+            data.append(Joint([nx, ny]))
+        elif dataType == 'stylolite':
+            data.append(Stylolite([nx, ny]))
+        else:
+            raise Exception('data type {dataType} is unknown!')
+
+
 # -------------------------------------------
 
 def main():
@@ -165,7 +190,8 @@ def main():
 
     monteCarlo(data, 10000)
     plotDomain(data, 20)
-    # plotRotateS3()
+    plotRotateS3()
+
 
 # -------------------------------------------
 
--- invert-function.py
+++ invert-function.py
@@ -1,7 +1,7 @@
 #
 # PROCEDURAL programming in Python
 #
- 
+
 import math
 import random as rnd
 import functools
@@ -12,51 +12,63 @@ from typing import Callable
 Vector = tuple[float, float]
 Stress = tuple[tuple[float, float], tuple[float, float]]
 
+
 def normalize(n: Vector) -> Vector:
     l = math.sqrt(n[0] ** 2 + n[1] ** 2)
     if l != 0:
-        return [n[0] / l, n[1] / l]
+        return n[0] / l, n[1] / l
     else:
         return n
-    
-def deg2rad(a: float) -> float :
-    return a*math.pi/180
+
+
+def deg2rad(a: float) -> float:
+    return a * math.pi / 180
+
 
 def dot(n1: Vector, n2: Vector) -> float:
-    return n1[0]*n2[0] + n1[1]*n2[1]
+    return n1[0] * n2[0] + n1[1] * n2[1]
 
-def lerp(v0: float, v1: float, t: float) -> float :
+
+def lerp(v0: float, v1: float, t: float) -> float:
     return (1 - t) * v0 + t * v1
+
 
 class PrincipalDirections:
     S1: Vector
     S3: Vector
+
     def __init__(self, S1: Vector, S3: Vector) -> None:
         self.S1, self.S3 = S1, S3
+
 
 class Data:
     cost: Callable
     n: Vector
+
     def __init__(self, n: Vector, cost: Callable) -> None:
         self.cost = cost
         self.n = n
 
-def principalDirections(theta: float, k: float) -> PrincipalDirections :
+
+def principalDirections(theta: float, k: float) -> PrincipalDirections:
     a = deg2rad(theta)
     c, s = math.cos(a), math.sin(a)
-    xx, xy, yy = k*s*s, k*c*s, k*c*c
+    xx, xy, yy = k * s * s, k * c * s, k * c * c
     trace = xx + yy
-    discri = math.sqrt(trace*trace - 4*(xx * yy - xy*xy))
+    discri = math.sqrt(trace * trace - 4 * (xx * yy - xy * xy))
     # Decreasing order according to the eigen values
-    S1 = normalize([xy, (trace + discri) / 2 - xx])
-    S3 = normalize([xy, (trace - discri) / 2 - xx])
+    S1 = normalize( (xy, (trace + discri) / 2 - xx) )
+    S3 = normalize( (xy, (trace - discri) / 2 - xx) )
     return PrincipalDirections(S1, S3)
-        
+
+
 def costJoint(n: Vector, r: PrincipalDirections) -> float:
     return 1.0 - math.fabs(dot(n, r.S3))
 
+
 def costStylo(n: Vector, r: PrincipalDirections) -> float:
     return 1.0 - math.fabs(dot(n, r.S1))
+
 
 # Monte Carlo simulation (random)
 def mc(data: list[Data], n: int = 5000):
@@ -69,18 +81,19 @@ def mc(data: list[Data], n: int = 5000):
             cost, theta, k = c, THETA, K
             print(theta, k, c)
 
+
 # Going further, plot the stress domain to check the solution
 def plotDomain(data: list[Data], n: int):
-    Z = np.zeros(shape=(n,n))
+    Z = np.zeros(shape=(n, n))
     min = 0.001
     max = 0.99
     for i in range(0, n):
-        k = lerp(min, max, i/(n-1))
+        k = lerp(min, max, i / (n - 1))
         for j in range(0, n):
-            theta = lerp(0, 180, j/(n-1))
+            theta = lerp(0, 180, j / (n - 1))
             remote = principalDirections(theta, k)
-            Z[j][i] = functools.reduce(lambda a, b: a+b, [x.cost(x.n, remote) for x in data]) / len(data)
-            
+            Z[j][i] = functools.reduce(lambda a, b: a + b, [x.cost(x.n, remote) for x in data]) / len(data)
+
     X, Y = np.meshgrid(np.linspace(min, max, n), np.linspace(0, 180, n))
     levels = np.linspace(Z.min(), Z.max(), 50)
     cmap = 'jet'
@@ -93,23 +106,24 @@ def plotDomain(data: list[Data], n: int):
     fig.colorbar(plt.cm.ScalarMappable(cmap=cmap), ax=ax, orientation='vertical', label='Cost')
     plt.show()
 
+
 def plotRotateS3():
     x = []
-    
+
     yj = []
     ys = []
-    
+
     font = {'family': 'serif',
-        'color':  'darkred',
-        'weight': 'normal',
-        'size': 16,
-        }
-    
+            'color': 'darkred',
+            'weight': 'normal',
+            'size': 16,
+            }
+
     for angle in range(0, 91, 1):
         x.append(angle)
         dirs = principalDirections(angle, 1)
-        yj.append( costJoint([0,1], dirs) )
-        ys.append( costStylo([0,1], dirs) )
+        yj.append(costJoint([0, 1], dirs))
+        ys.append(costStylo([0, 1], dirs))
 
     fig, ax = plt.subplots()
     ax.plot(x, yj, label='Joint')
@@ -124,17 +138,22 @@ def plotRotateS3():
     plt.ylabel('Cost', fontdict=font)
     # ---
     plt.show()
+
+
 # -------------------------------------------
 
 data: list[Data] = []
 
+
+
 def addData(file: str, costFct: Callable):
     f = open(file, "r")
-    for line in f: # for each line
+    for line in f:  # for each line
         tokens = line.removesuffix('\n').split(' ')
         nx = float(tokens[0])
         ny = float(tokens[1])
-        data.append( Data([nx, ny], costFct) )
+        data.append(Data([nx, ny], costFct))
+
 
 addData("matelles-joints.txt", costJoint)
 addData("matelles-stylolites.txt", costStylo)
